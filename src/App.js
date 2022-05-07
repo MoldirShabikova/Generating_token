d46db03d130587c9e9efbc5e1771b85c858c4d9c
moldir <moldirshb@gmail.com>
2022-05-06 22:19:27 -0400

    added validation

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
 import React, { Component } from 'react';
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
+      name: '',
       token: null,
       email: 't@t.com',
       password: '123',
       fieldsEmpty: false,
+      notFound: false,
     };
   }
 
   componentDidMount() {
     let token = sessionStorage.getItem('token');
+    let name = sessionStorage.getItem('name');
 
     if (token) {
-      console.log('session contains token');
+      this.setState({ token, name });
     }
   }
 
   handleFormSubmit = (event) => {
     event.preventDefault();
+    const { email, password } = this.state;
     const nameValue = event.target.name.value;
     const emailValue = event.target.email.value;
     const pwdValue = event.target.password.value;
 
     if (nameValue !== '' && emailValue !== '' && pwdValue !== '') {
-      console.log('auth');
+      if (emailValue === email && pwdValue === password) {
+        const token = emailValue + pwdValue; // t@t.com123
+        // const token = uuidv4();
+        sessionStorage.setItem('token', token);
+        sessionStorage.setItem('name', nameValue);
+        this.setState({ token: 'abc', name: nameValue });
+      } else {
+        this.setState({ notFound: true });
+        setTimeout(() => {
+          this.setState({ notFound: false });
+        }, 2000);
+      }
     } else {
       this.setState({ fieldsEmpty: true });
       setTimeout(() => {
@@ -44,19 +59,22 @@ export default class App extends Component {
   };
 
   render() {
-    const { token, fieldsEmpty } = this.state;
+    const { token, fieldsEmpty, name, notFound } = this.state;
     return (
-      <div className='App'>
+      <div>
         {token ? (
           <div className='container'>
             <p>Dashboard</p>
-            <p>Dear Name, welcome to your profile!</p>
+            <p>Dear {name}, welcome to your profile!</p>
             <Button onClick={this.handleLogout}>Logout</Button>
           </div>
         ) : (
           <div className='container'>
             {fieldsEmpty ? (
               <div className='error'>Please fill the fields first!</div>
+            ) : null}
+            {notFound ? (
+              <div className='error'>User is not found in our system!</div>
             ) : null}
             <Form inline onSubmit={this.handleFormSubmit}>
               <FormGroup floating>
